dd6fdf1d7637ba44f551f6d89a290abc4c644b2f
nolazheng <nola.zheng@zoomo.space>
2024-11-02 21:37:23 +0800

    feat: update eslint version & add decamelize-keys package

--- eslint.config.js
+++ eslint.config.js
@@ -26,6 +26,7 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
     },
   }
 );
